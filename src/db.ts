faf1d2c349bf393e9efe1796dde079c58d11d9a8
Cristian Rojas <crisdarroca@hotmail.com>
2023-04-09 23:32:16 -0500

    feat: environment variables

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2'
+import { envConfig } from './config'
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'toor',
-  database: 'companydb',
-  port: 3308
+  host: envConfig.DB_HOST,
+  user: envConfig.DB_USER,
+  password: envConfig.DB_PASSWORD,
+  database: envConfig.DB_DATABASE,
+  port: +envConfig.DB_PORT
 })
 
 export const promisePool = pool.promise()
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes'
-import indexRoutes from './routes/index.routes'
+import { envConfig } from './config'
+import app from './app'
 
-const app = express()
+app.listen(envConfig.PORT)
 
-app.use(express.json())
-
-app.use('/api', employeesRoutes)
-app.use(indexRoutes)
-
-app.use((_req, res) => {
-  res.status(404).send({ message: 'Endpoint not found' })
-})
-app.listen(3000)
-console.log('server is running on port 3000')
+console.log(`server is running on port ${envConfig.PORT}`)
