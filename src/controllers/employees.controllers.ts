a929f9b66f4ded37501213b6d21d54a70137ed8e
Cristian Rojas <crisdarroca@hotmail.com>
2023-04-09 22:35:52 -0500

    feat: validate errors

--- src/controllers/employees.controllers.ts
+++ src/controllers/employees.controllers.ts
@@ -3,70 +3,90 @@ import type mysql from 'mysql2'
 import { promisePool } from '../db'
 
 export const getEmployees: RequestHandler = async (_req, res) => {
-  const [row]: mysql.RowDataPacket[] = await promisePool.query(
-    'SELECT * FROM employee;'
-  )
-  res.status(200).send(row)
+  try {
+    const [row]: mysql.RowDataPacket[] = await promisePool.query(
+      'SELECT * FROM employee;'
+    )
+    res.status(200).send(row)
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong' })
+  }
 }
 
 export const getEmployee: RequestHandler = async (req, res) => {
-  const { id } = req.params
-  const [row]: [mysql.RowDataPacket[]] = await promisePool.query(
-    'SELECT * FROM employee WHERE id = ?;',
-    [id]
-  )
+  try {
+    const { id } = req.params
+    const [row]: [mysql.RowDataPacket[]] = await promisePool.query(
+      'SELECT * FROM employee WHERE id = ?;',
+      [id]
+    )
 
-  if (row.length <= 0) {
-    res.status(404).send({
-      message: 'Employee not found'
-    })
-    return
+    if (row.length <= 0) {
+      res.status(404).send({
+        message: 'Employee not found'
+      })
+      return
+    }
+
+    res.status(200).send(row)
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong' })
   }
-
-  res.status(200).send(row)
 }
 
 export const createEmployees: RequestHandler = async (req, res) => {
-  const { name, salary } = req.body
-  const [row]: [mysql.OkPacket] = await promisePool.query(
-    'INSERT INTO employee (name,salary) VALUES (?,?)',
-    [name, salary]
-  )
-  console.log({ id: row.insertId, name, salary })
-  res.status(201).send({ id: row.insertId, name, salary })
+  try {
+    const { name, salary } = req.body
+    const [row]: [mysql.OkPacket] = await promisePool.query(
+      'INSERT INTO employee (name,salary) VALUES (?,?)',
+      [name, salary]
+    )
+    console.log({ id: row.insertId, name, salary })
+    res.status(201).send({ id: row.insertId, name, salary })
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong' })
+  }
 }
 
 export const updateEmployees: RequestHandler = async (req, res) => {
-  const { id } = req.params
-  const { name, salary } = req.body
+  try {
+    const { id } = req.params
+    const { name, salary } = req.body
 
-  const [row]: [mysql.OkPacket] = await promisePool.query(
-    'UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE id = ?',
-    [name, salary, id]
-  )
+    const [row]: [mysql.OkPacket] = await promisePool.query(
+      'UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE id = ?',
+      [name, salary, id]
+    )
 
-  if (row.affectedRows <= 0) {
-    res.status(404).send({ message: 'Employee not found' })
-    return
+    if (row.affectedRows <= 0) {
+      res.status(404).send({ message: 'Employee not found' })
+      return
+    }
+
+    const [userUpdated] = await promisePool.query(
+      'SELECT * FROM employee WHERE id = ?',
+      [id]
+    )
+    res.status(200).send(userUpdated)
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong' })
   }
-
-  const [userUpdated] = await promisePool.query(
-    'SELECT * FROM employee WHERE id = ?',
-    [id]
-  )
-  res.status(200).send(userUpdated)
 }
 
 export const deleteEmployees: RequestHandler = async (req, res) => {
-  const { id } = req.params
-  const [row]: [mysql.OkPacket] = await promisePool.query(
-    'DELETE FROM employee WHERE id = ?',
-    [id]
-  )
+  try {
+    const { id } = req.params
+    const [row]: [mysql.OkPacket] = await promisePool.query(
+      'DELETE FROM employee WHERE id = ?',
+      [id]
+    )
 
-  if (row.affectedRows <= 0) {
-    res.status(404).send({ message: 'Employee not found' })
-    return
+    if (row.affectedRows <= 0) {
+      res.status(404).send({ message: 'Employee not found' })
+      return
+    }
+    res.status(200).send(`delete employee with id ${id}`)
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong' })
   }
-  res.status(200).send(`delete employee with id ${id}`)
 }
